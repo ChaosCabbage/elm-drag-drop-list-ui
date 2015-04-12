8f7882aff962ce1b00f379726386ff62641c1ecb
Paul Courts <puke_4@hotmail.com>
2015-04-12 22:32:06 +0100

    Shuffle items up and down as you drag over list.
    Drag an item up and down and the list will make room for you to drop it
    in.
    Problems:
    -Position where the switch happens isn't ideal.
    -hover signal doesn't update properly when you move things around
    automatically.
    - Still doesn't emit any signals when you drop an item.

--- DragDropList.elm
+++ DragDropList.elm
@@ -2,16 +2,20 @@ module DragDropList (main) where
 
 import Color
 import Graphics.Element (..)
+import Graphics.Collage (..)
 import Graphics.Input
 import List
 import Text
 import DragAndDrop
 import Signal
 import Debug
+import Array
+
+-- Signals
 
 hover = Signal.channel Nothing
 
-hoverWatch = Debug.watch "hover" (Signal.subscribe hover)
+-- Models
 
 type DragItem = Dragging Int (Int,Int) | NotDragging
 
@@ -20,13 +24,16 @@ type alias Model =
   , gap : Element
   }
 
+-- View
+
 dragPadElement : Element
 dragPadElement = spacer 30 40 |> color Color.black
 
 dragPad id =
-  let action hovering = Debug.watch "hover" <| case hovering of
-    True -> Just id
-    False -> Nothing
+  let action hovering = Debug.watch "hover" <| 
+    case hovering of
+      True -> Just id
+      False -> Nothing
   in
     Graphics.Input.hoverable (Signal.send hover << action) dragPadElement
 
@@ -43,19 +50,10 @@ sameSizedSpacer element =
   let (x,y) = sizeOf element
   in spacer x y
 
-listWithFloater list gapId (xoff,yoff)=
-  let beforeGap = List.take gapId list
-      (gapElement::afterGap) = List.drop gapId list
-      (x,y) = sizeOf gapElement
-      pos = topLeftAt (absolute xoff) (absolute yoff)
-      floater = container (x+xoff) (y+yoff) pos gapElement
-  in
-    beforeGap ++ [floater] ++ afterGap
-
 gapElement model =
   let (dragW,dragH) = sizeOf dragPadElement
       (gapW,gapH) = sizeOf model.gap
-      maxWidth = List.foldr (\el -> widthOf el |> max) 0 model.list
+      maxWidth = List.map widthOf model.list |> List.maximum
   in
     container maxWidth gapH middle model.gap
     |> container (maxWidth + dragW) gapH midRight
@@ -65,26 +63,88 @@ listElements model =
   in
     List.map (flow right) <| listWithDragpads model.list
 
-view : Model -> DragItem -> Element
-view model dragging =
+insertionPoints model =
+  let gapHeight = heightOf model.gap
+      heightOfElement e = (heightOf e) + gapHeight
+      heights = List.map heightOfElement model.list
+  in List.take (List.length heights) (List.scanl (+) 0 heights)
+
+drawInsertionPoints model =
+  let points = insertionPoints model |> Debug.watch "insertion points"
+      drawLine = rect 100 5
+                  |> filled Color.red
+                  |> alpha 0.7
+      drawLineAt pos = drawLine |> move (0, ((toFloat h)/2) - toFloat pos)
+      h = if (List.isEmpty points) then
+            0
+          else
+            ((List.head <| List.reverse points) + 5)
+  in collage 100 h (
+    (List.map drawLineAt points)
+    ++
+    [rect 100 (toFloat h) |> filled Color.orange |> alpha 0.4]
+  )
+
+
+itemYPosition : Model -> Int -> Int
+itemYPosition model id =
+  List.head (List.drop id (insertionPoints model))
+
+currentInsertionPoint : Model -> Int -> Int -> Int
+currentInsertionPoint model gapId yOffset =
+  let points = insertionPoints model
+      originalTop = itemYPosition model gapId
+      pointIndexes = List.indexedMap (,) points
+      afterHeight = List.filter (\(_,x) -> x > originalTop+yOffset) pointIndexes
+  in
+    if (List.isEmpty afterHeight) then
+      (List.length pointIndexes)-1
+    else
+      fst (List.head afterHeight)
+
+
+listWithSpace list gapId insertionPoint =
+  let beforeSpace = List.take gapId list
+      (spaceElement::afterSpace) = List.drop gapId list
+      newList = beforeSpace ++ afterSpace
+      beforeInsertion = List.take insertionPoint newList
+      afterInsertion = List.drop insertionPoint newList
+  in
+    beforeInsertion ++ [sameSizedSpacer spaceElement] ++ afterInsertion
+
+drawListWithFloatingGap model dragging =
   let pureElements = listElements model
       elements = case dragging of
-          Dragging id (x,y) -> listWithFloater pureElements id (x,y)
+          Dragging id (x,y) ->
+            listWithSpace pureElements id
+                (Debug.watch "current insert point"
+                (currentInsertionPoint model id y))
           NotDragging -> pureElements
-  in flow down <| List.intersperse (gapElement model) elements
+      withGaps = List.intersperse (gapElement model) elements
+  in
+    flow down withGaps
+
+drawFloatingItem model dragging =
+  case dragging of
+    NotDragging -> empty
+    Dragging id (x,y) ->
+      let (xMax,yMax) = sizeOf <| drawListWithFloatingGap model dragging
+          element = flow right [List.head (List.drop id model.list)]
+          yPos = -((itemYPosition model id) + y + ((heightOf element) // 2) - (yMax//2))
+          floatingForm = toForm element |> move (toFloat x, toFloat yPos)
+      in collage xMax yMax [floatingForm]
 
 
 
-sampleElement =
-  container 200 100 middle (Text.plainText "What")
-    |> color Color.yellow
+view : Model -> DragItem -> Element
+view model dragging =
+  layers [
+    drawListWithFloatingGap model dragging,
+--    drawInsertionPoints model,
+    drawFloatingItem model dragging
+  ]
 
-sampleList =
-  List.repeat 7 sampleElement
 
-sampleGap =
-  spacer 100 10
-    |> color Color.red
 
 moveBy (dx,dy) (x,y) = (x + dx, y - dy)
 
@@ -98,10 +158,22 @@ dragDropUpdate m =
 dragStatus dragDrop movement =
   case dragDrop of
     Nothing                       -> NotDragging
-    Just (_, DragAndDrop.Release)  -> NotDragging
+    Just (_, DragAndDrop.Lift)    -> NotDragging
+    Just (_, DragAndDrop.Release) -> NotDragging
     Just (i, _)                   -> Dragging i movement
   |> Debug.watch "drag"
 
+
+sampleElement text =
+  container 200 100 middle (Text.plainText text)
+    |> color Color.yellow
+
+sampleList =
+  List.map sampleElement ["Who", "What", "When", "Where", "How"]
+
+sampleGap =
+  spacer 100 10
+    |> color Color.red
 
 main =
   let track = DragAndDrop.trackMany Nothing (Signal.subscribe hover)
